dcd0f12c55b7aa15e626ca78ffb1aeff2d479f4a
Carla0504 <carla.fernandez86@educa.madrid.org>
2025-05-08 12:16:27 +0200

    Web

---
+++ web/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdownBtn = document.querySelector('.dropdown-btn');
+    const dropdownContent = document.querySelector('.dropdown-content');
+
+    dropdownBtn.addEventListener('click', () => {
+        dropdownContent.classList.toggle('show');
+    });
+});
